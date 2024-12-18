0bb404c24e588fe0517f977893824d3cf59ed751
ItachiVN <minhthuan1091996@gmail.com>
2024-12-18 15:07:11 +0700

    fix: update wakeup source from PC13 to PA12 in main.c

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 #include "hal.h"
 
 #define APP_TEST_PWM            	  (1)
-#define APP_TEST_WAKEUP_PC13        (1)
+#define APP_TEST_WAKEUP_PA12        (1)
 /******************************************************************************
 * Module Preprocessor Macros
 *******************************************************************************/
@@ -23,20 +23,20 @@
 * Function Definitions
 *******************************************************************************/
 
-#if (APP_TEST_WAKEUP_PC13 != 0)
+#if (APP_TEST_WAKEUP_PA12 != 0)
 /**
   * @brief PB13 interrupt configured as wakeup source
   */
-void pc13_wakeup_init(void)
+void pa12_wakeup_init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
-  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
 
-  /*Configure GPIO pin PC13 to wake up the device from stop mode */
-  GPIO_InitStruct.Pin = GPIO_PIN_13;
+  /*Configure GPIO pin PA12 to wake up the device from stop mode */
+  GPIO_InitStruct.Pin = GPIO_PIN_12;
   GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
 
   /* EXTI interrupt init*/
@@ -52,13 +52,13 @@ void EXTI4_15_IRQHandler(void)
  /* USER CODE BEGIN EXTI4_15_IRQn 0 */
 
  /* USER CODE END EXTI4_15_IRQn 0 */
- HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_13);
+ HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_12);
  /* USER CODE BEGIN EXTI4_15_IRQn 1 */
 
  /* USER CODE END EXTI4_15_IRQn 1 */
 }
 
-#endif /* End of (APP_TEST_WAKEUP_PC13 != 0)) */
+#endif /* End of (APP_TEST_WAKEUP_PA12 != 0)) */
 
 #if (APP_TEST_PWM != 0)
 int __InitPWM();
@@ -83,9 +83,9 @@ int main(void)
 		printf("HAL init failed");
 		return -1;
 	}
-#if (APP_TEST_WAKEUP_PC13 != 0)
-    pc13_wakeup_init();
-#endif /* End of (APP_TEST_WAKEUP_PC13 != 0) */
+#if (APP_TEST_WAKEUP_PA12 != 0)
+    pa12_wakeup_init();
+#endif /* End of (APP_TEST_WAKEUP_PA12 != 0) */
 
     while(1)
     {
